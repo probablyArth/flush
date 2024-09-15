78d8d49827fb3298c29e6e7d14a4f5b97fcfb98b
Arth <probablyarth@gmail.com>
2024-09-15 19:09:39 +0530

    add test

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,7 +1,18 @@
+import { Flush } from '../src/index';
+
 const sum = (a: number, b: number) => a + b;
 
 describe('blah', () => {
   it('works', () => {
     expect(sum(1, 1)).toEqual(2);
+  });
+});
+
+describe('test default console processor', () => {
+  it('prints to console', () => {
+    const spy = jest.spyOn(console, 'log');
+    const flush = new Flush('DEBUG');
+    flush.flush({ level: 'DEBUG', message: 'test' });
+    expect(spy).toHaveBeenCalled();
   });
 });
